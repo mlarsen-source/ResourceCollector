c7e66935aa617473cc8318dd6744e22d0968d396
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-16 13:35:58 -0700

    modified

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,8 +1,24 @@
+import PlayerCard from "./PlayerCard.jsx"
+import Store from "./Store.jsx"
+
 export default function Game() {
   
   return (
 
+    < PlayerCard />
+
+    <main> 
+      <img onlClick= src=></img>
+      <p>Wood</p>
+      img onClick= src=></img>
+      <p>Stone</p>
+     <img onClick src=></img>
+      <p>Iron</p>
+    </main>
+
     <h1>This is the game</h1>
+
+    <Store />
 
   )
 }
