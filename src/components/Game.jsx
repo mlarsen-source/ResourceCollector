816ee928bb769bf252de02c47dcbb522335c1d82
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-10-16 13:41:49 -0700

    added images

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,21 +4,21 @@ import Store from "./Store.jsx"
 export default function Game() {
   
   return (
+    <>
+      < PlayerCard />
 
-    < PlayerCard />
+      <main> 
+        <img onClick src="https://png.pngtree.com/png-vector/20240327/ourmid/pngtree-quirky-retro-style-cartoon-wooden-log-png-image_12215791.png"></img>
+        <p>Wood</p>
+        <img onClick src="https://cdn.creazilla.com/cliparts/3152335/stone-clipart-xl.png"></img>
+        <p>Stone</p>
+        <img onClick src="https://www.clipartmax.com/png/middle/89-893353_iron-and-steel-clip-art.png"></img>
+        <p>Iron</p>
+      </main>
 
-    <main> 
-      <img onlClick= src=></img>
-      <p>Wood</p>
-      img onClick= src=></img>
-      <p>Stone</p>
-     <img onClick src=></img>
-      <p>Iron</p>
-    </main>
+      <h1>This is the game</h1>
 
-    <h1>This is the game</h1>
-
-    <Store />
-
+      <Store />
+    </>
   )
 }
