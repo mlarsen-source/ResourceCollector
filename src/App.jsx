e89c350f56b7893ede7439dea35396cd583fc2e9
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-16 13:03:23 -0700

    added basic component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Game from "./componentsGame"
 
 function App() {
   
   return (
     <>
-     
+    <Header />
+    <Game />
+    <Footer />
     </>
   )
 }
